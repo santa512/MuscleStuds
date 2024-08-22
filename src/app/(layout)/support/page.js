7ed827cf4c1558fa690fb4687e04fdf38288b5e1
santa <157872827+santa512@users.noreply.github.com>
2024-08-22 11:58:25 -0400

    debug-quote

--- src/app/(layout)/support/page.js
+++ src/app/(layout)/support/page.js
@@ -1,21 +1,21 @@
 export default function Support() {
-	return (
-		<div className="bg-secondary p-6 pt-2">
-			<h2 className="text-xl text-center py-5 uppercase text-gray-400">Support</h2>
-			<p className="text-gray-300 leading-loose">
-				<b>Am I able to save the images onto my computer?</b><br/>
-				Yes. You can save images by right clicking on the image and selecting the &apos;save image&apos; command.<br/>
-				<b>Are the videos available to buy on DVD?</b><br/>
-				Yes.<br/>
-				<b>Are your videos DRM protected?</b><br/>
-				Our videos are 100% DRM-Free so you will have no problem in viewing our videos after you order it. This also means that they are fully Mac compatible.<br/>
-				<b>Can I watch videos on iPhone and iPad?</b><br/>
-				Yes. Our videos are fully compatible to watch on iPad and iPhone.<br/>
-				<b>What format are the movies in?</b><br/>
-				Movies are available in MP4 format.<br/>
-				<b>Have some other question?</b><br/>
-				Please contact us at our contact form.
-			</p>
-		</div>
-	)
+  return (
+    <div className="bg-secondary p-6 pt-2">
+      <h2 className="text-xl text-center py-5 uppercase text-gray-400">Support</h2>
+      <p className="text-gray-300 leading-loose">
+        <b>Am I able to save the images onto my computer?</b><br/>
+        Yes. You can save images by right clicking on the image and selecting the 'save image' command.<br/>
+        <b>Are the videos available to buy on DVD?</b><br/>
+        Yes.<br/>
+        <b>Are your videos DRM protected?</b><br/>
+        Our videos are 100% DRM-Free so you will have no problem in viewing our videos after you order it. This also means that they are fully Mac compatible.<br/>
+        <b>Can I watch videos on iPhone and iPad?</b><br/>
+        Yes. Our videos are fully compatible to watch on iPad and iPhone.<br/>
+        <b>What format are the movies in?</b><br/>
+        Movies are available in MP4 format.<br/>
+        <b>Have some other question?</b><br/>
+        Please contact us at our contact form.
+      </p>
+    </div>
+  );
 }
